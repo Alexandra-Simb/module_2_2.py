868861a715929ada496d0f28f2c0dd7948ab1652
Alexandra-Simb <simbirskaya.asya@mail.ru>
2024-08-17 17:46:50 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,8 @@
+first = 12
+second = 34
+third = 12
+if first == second and second == third and first == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else: print(0)
